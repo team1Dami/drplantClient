2703581a01257995c4ae7654c6e7fa7437a6afb2
eneko <eneko@DESKTOP-3ULA5MQ>
2021-02-07 19:51:13 +0100

    Cambios DIN

--- src/drPlant/controller/EquipmentViewController.java
+++ src/drPlant/controller/EquipmentViewController.java
@@ -36,74 +36,135 @@ import javafx.stage.WindowEvent;
 import javax.ws.rs.core.GenericType;
 
 /**
+ * Controller class for equipment' management view . It contains event handlers
+ * and initialization code for the view defined in EquipmentView.fmxl file.
  *
  * @author Eneko
  */
 public class EquipmentViewController {
 
+    /**
+     * The logger to catch the info of the view and it's events
+     */
     private static final Logger LOOGER = Logger.getLogger("drPlant.controller.EquipmentViewController");
 
+    /**
+     * The user who has logged in in the aplication
+     */
     private static User user;
 
     /**
+     * A method to get the user of this stage
      *
-     * @return
+     * @return The User of this stage
      */
     public static User getUser() {
         return user;
     }
 
     /**
+     * A method to set the user of this stage
      *
-     * @param user
+     * @param user The user information that you want to set in
      */
     public static void setUser(User user) {
         EquipmentViewController.user = user;
     }
 
     //Declaration of attributes
+    /**
+     * The stage of this window
+     */
     @FXML
     private Stage stage;
+    /**
+     * The button to search equipments by their use, name or both
+     */
     @FXML
     private Button btnSearch;
+    /**
+     * The button to add a new equipment, it will rise a new window to introduce
+     * the information
+     */
     @FXML
     private Button btnAdd;
+    /**
+     * The button to edit the selected equipment in the table that will rise a
+     * new window with the actual information
+     */
     @FXML
     private Button btnEdit;
+    /**
+     * The button to delete the selected equipment on the table
+     */
     @FXML
     private Button btnDelete;
+    /**
+     * A choice box with the possible uses in orther to search the equipments
+     * with that use
+     */
     @FXML
     private ChoiceBox choiceBoxFiltros;
+    /**
+     * A text field to introduce the equipment name you want to search for
+     */
     @FXML
     private TextField txtSearch;
+    /**
+     * The table with the equipents where you can see them
+     */
     @FXML
     private TableView equipmentTable;
+    /**
+     * The table colum wich contains the name of the equipment
+     */
     @FXML
     private TableColumn nameCol;
+    /**
+     * The table colum wich contains the description of the equipment
+     */
     @FXML
     private TableColumn descriptionCol;
+    /**
+     * The table colum wich contains the use of the equipment
+     */
     @FXML
     private TableColumn useCol;
+    /**
+     * The table colum wich contains the price of the equipment
+     */
     @FXML
     private TableColumn priceCol;
 
+    /**
+     * The menu controller to load the menu
+     */
     @FXML
     private MenuController menuControllerController;
 
+    /**
+     * A boolean to know if the user that is logged is an admin or not
+     */
     private boolean isAdmin;
+
+    /**
+     * A observable list where the equipments that returns the database are load
+     */
     private ObservableList<Equipment> equipments;
 
     /**
+     * Returns the boolean isAdmin is true or false
      *
-     * @return
+     * @return isAdmin boolean value
      */
     public boolean isIsAdmin() {
         return isAdmin;
     }
 
     /**
+     * Set the isAdmin boolean to true or false
      *
-     * @param isAdmin
+     * @param isAdmin A boolean
      */
     public void setIsAdmin(boolean isAdmin) {
         this.isAdmin = isAdmin;
@@ -112,7 +173,7 @@ public class EquipmentViewController {
     /**
      * Set the stage of the view
      *
-     * @param stage
+     * @param stage Stage 
      */
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -121,17 +182,21 @@ public class EquipmentViewController {
     /**
      * Initialize the view and set actions on different widgets
      *
-     * @param root
-     * @param u
+     * @param root The parent value
+     * @param u The user of the previus stage
      */
     public void initStage(Parent root, User u) {
+        //Get the User witch has logged in
         menuControllerController.setUser(u);
         menuControllerController.setStageOld(stage);
+        //Gets the privilege of the user to set the isAdmin value if the privilege is ADMIN
+        //the isAdmin will be set to true, if is USER the isAdmin will be false
         if (u.getPrivilege().equals(UserPrivilege.ADMIN)) {
             setIsAdmin(true);
         } else {
             setIsAdmin(false);
         }
+        //Set stage properties
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setResizable(false);
@@ -139,27 +204,37 @@ public class EquipmentViewController {
         stage.setOnShowing(this::handleWindowShowing);
         stage.setOnCloseRequest(this::setOncloseRequest);
         stage.show();
-
+        //Add all the equipments of the database to the table
         setTableValues();
 
     }
 
+    /**
+     * Initializes window state. It implements behavior for WINDOW_SHOWING type
+     * event.
+     *
+     * @param event The window event
+     */
     private void handleWindowShowing(WindowEvent event) {
 
+        //Set the promp text to the search field
         txtSearch.setPromptText("Introduce el equipamiento que quieres buscar");
         txtSearch.focusedProperty().addListener(this::focusChanged);
 
+        //Set the columns of the tables
         nameCol.setCellValueFactory(new PropertyValueFactory<>("equipment_name"));
         descriptionCol.setCellValueFactory(new PropertyValueFactory<>("equipment_description"));
         useCol.setCellValueFactory(new PropertyValueFactory<>("use"));
         priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
 
+        //Set the choicebox items to the posible uses you can search for
         choiceBoxFiltros.setItems(FXCollections.observableArrayList("", "General", "Sustrato", "Riego"));
         choiceBoxFiltros.setValue("");
-        
-        priceCol.setStyle( "-fx-alignment: CENTER-RIGHT;");
 
-        // idea: en función de la selección del choice box se mostrará un promp text u otro
+        //Set the price text allignment to the right in order to compare better the price
+        priceCol.setStyle("-fx-alignment: CENTER-RIGHT;");
+
+        //Looks if isAdmin is true or false in order to show the buttons of editing, adding and deleting 
         if (!isAdmin) {
             btnAdd.setVisible(isAdmin);
             btnEdit.setVisible(isAdmin);
@@ -171,6 +246,7 @@ public class EquipmentViewController {
             btnDelete.setVisible(isAdmin);
         }
 
+        //A listener to the table to know if it's clicked
         equipmentTable.setOnMouseClicked((MouseEvent event2) -> {
             if (event2.getButton().equals(MouseButton.PRIMARY)) {
                 btnDelete.setDisable(false);
@@ -178,6 +254,7 @@ public class EquipmentViewController {
             }
         });
 
+        //Set the event handlers to the respective buttons
         btnSearch.setOnAction(this::handleSearchAction);
         btnAdd.setOnAction(this::handleAddAction);
         btnEdit.setOnAction(this::handleEditAction);
@@ -185,39 +262,48 @@ public class EquipmentViewController {
     }
 
     /**
-     * Recoge el texto que se ha introducido en el textFieldBuscador y la opción
-     * seleccionada en el choice box en caso de encontrar la plaga la mostrará
-     * en la lista.
+     * Gets the text that is in the textfield of search and the selected option
+     * in the choice box of uses, in case that the searched quipment exists it
+     * will show all the equipments found with that parameters
      *
-     * @param event event - onClick event
+     * @param event The ActionEvent object for the event.
      */
     @FXML
     private void handleSearchAction(ActionEvent event) {
         String filtroUso = (String) choiceBoxFiltros.getValue();
         String buscador = txtSearch.getText();
+        //It erase the white spaces before and after the text of the sear field
         buscador = buscador.replaceAll("^\\s*", "");
         buscador = buscador.replaceAll("\\s*$", "");
+        //It checks what filters are activated 
         if (!filtroUso.isEmpty() && buscador.isEmpty()) {
+            //Search by use
             setTableValuesWithFilters(filtroUso);
         } else if (!filtroUso.isEmpty() && !buscador.isEmpty()) {
+            //Search by name and use
             setTableValuesWithFilters(filtroUso, buscador);
         } else if (filtroUso.isEmpty() && !buscador.isEmpty()) {
+            //Search by name
             setTableValuesWithText(buscador);
         } else {
+            //Search without filters
             setTableValues();
         }
     }
 
     /**
-     * Se abrirá la ventana modal de infoPlague.fxml todos los campos (nombre
-     * científico,nombre común, descripción, control, remedio y tipo) editables.
+     * It will show the EquipmentDetailsView.fxml with all the fields empty in
+     * order to introduce the new equipment information, after you save the
+     * information it will add the new equipment to the database and refresh the
+     * table
      *
-     * @param event
+     * @param event The ActionEvent object for the event.
      */
     @FXML
     private void handleAddAction(ActionEvent event) {
         Parent root;
         try {
+            //Load the modal window to add a new equipment
             Stage newstage = new Stage();
             EquipmentDetailsController controller = new EquipmentDetailsController();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/drPlant/view/EquipmentDetails.fxml"));
@@ -234,28 +320,34 @@ public class EquipmentViewController {
         } catch (Exception e) {
             LOOGER.log(Level.SEVERE, "An exception ocurred", e.getMessage());
         }
+        //Refresh the table with the new equipment
         setTableValues();
     }
 
     /**
-     * Se abrirá la ventana modal de infoPlague.fxml campos (nombre común,
-     * descripción, control, remedio y tipo) editables.
+     * It will show the EquipmentDetailsView.fxml with all the fields set with
+     * the selected equipment information, in case there isn't a selected row it
+     * will show an alert to notify the user that he must select a row, to
+     * introduce the new equipment information, after you save the information
+     * it will update the equipment to the database and refresh the table
      *
-     * @param event
+     * @param event The ActionEvent object for the event.
      */
     @FXML
     private void handleEditAction(ActionEvent event) {
         try {
             Equipment equip = (Equipment) equipmentTable.getSelectionModel().getSelectedItem();
+            //Check if a row is selected
             if (equip.getEquipment_name().isEmpty() && equip.getEquipment_description().isEmpty()) {
                 Alert alert = new Alert(Alert.AlertType.WARNING,
-                        "Primero seleccione una fila de la tabla", ButtonType.OK); //alert to ask the user to confirm
+                        "Primero seleccione una fila de la tabla", ButtonType.OK); //Alert to ask the user to confirm
                 alert.showAndWait();
                 btnDelete.setDisable(true);
                 btnEdit.setDisable(true);
             }
             Parent root;
             try {
+                //Load the modal window to edit the selected equipment
                 Stage newstage = new Stage();
                 EquipmentDetailsController controller = new EquipmentDetailsController();
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/drPlant/view/EquipmentDetails.fxml"));
@@ -271,6 +363,7 @@ public class EquipmentViewController {
             } catch (Exception e) {
                 LOOGER.log(Level.SEVERE, "Exception while loading the controller", e.getMessage());
             }
+            //Refresh the table with the equipment edited updated
             setTableValues();
         } catch (NullPointerException ex) {
             LOOGER.log(Level.SEVERE, "No row selected", ex.getMessage());
@@ -284,21 +377,25 @@ public class EquipmentViewController {
     }
 
     /**
-     * Se mostrará un alert indicando la acción con las opciones SÍ / NO. En
-     * caso de pulsar SÍ se eliminará la plaga seleccionada. Método de la capa
-     * de lógica al que se llama: remove(); En caso de pulsar NO se cerrará el
-     * alert.
+     * It will show an alert to warn the user about the delete and the name of
+     * the equipment is going to delete if the user want to delete it, he will
+     * click on the OK button and the equipment will be removed from the
+     * database, then the table will refresh with the new list of equipments in
+     * case there are in the database. If the user clicks on the CANCEL button,
+     * the warning will close as nothing happend
      *
-     * @param event
+     * @param event The ActionEvent object for the event.
      */
     @FXML
     private void handleDeleteAction(ActionEvent event) {
         try {
+            //Gets the selected item
             Equipment equip = (Equipment) equipmentTable.getSelectionModel().getSelectedItem();
 
             Alert alert = new Alert(Alert.AlertType.WARNING,
                     "¿Desea borrar el equipamiento " + equip.getEquipment_name() + "?", ButtonType.OK, ButtonType.CANCEL); //alert to ask the user to confirm
             alert.showAndWait();
+            //If the button clicked is OK it will delete the selected item
             if (!alert.getResult().getButtonData().isCancelButton()) {
                 EquipmentManagerFactory.getEquipmentManager().remove(equip.getId_equipment().toString());
                 setTableValues();
@@ -314,20 +411,22 @@ public class EquipmentViewController {
     }
 
     /**
-     * Method that asks when you press the x if you want to exit of the
-     * application if you press OK the application will be closed otherwhise you
-     * stay in the PlagueView
+     * Method that asks when you want to close the window if you want to exit of
+     * the application if you press OK the application will be closed otherwhise
+     * you stay in the EquipmentView
      *
-     * @param we
+     * @param we The ActionEvent object for the event.
      */
     private void setOncloseRequest(WindowEvent we) {
         Alert alert;
         try {
+            //Alert to ask the user to confirm
             alert = new Alert(Alert.AlertType.WARNING,
-                    "¿Desea salir de la aplicacion?", ButtonType.OK, ButtonType.CANCEL);//alert to ask the user to confirm
+                    "¿Desea salir de la aplicacion?", ButtonType.OK, ButtonType.CANCEL);
             alert.showAndWait();
             if (alert.getResult().getButtonData().isCancelButton()) {
-                we.consume();//do as nothing has happen
+                //Do as nothing has happen
+                we.consume();
             }
         } catch (Exception ex) {
             LOOGER.log(Level.SEVERE,
@@ -340,16 +439,23 @@ public class EquipmentViewController {
     }
 
     /**
+     * NOT WORKING (EMPTY) Is not used and it's empty
      *
-     * @param observable
-     * @param oldValue
-     * @param newValue
+     * @param observable NOT WORKING
+     * @param oldValue NOT WORKING
+     * @param newValue NOT WORKING
      */
     public void focusChanged(ObservableValue observable, Boolean oldValue,
             Boolean newValue) {
 
     }
 
+    /**
+     * This method gets all the equipments stored in the database and load them
+     * to the equipment table, it is used to load the equipment for the first
+     * time when the window is showed or to load the changes after a
+     * modification on the database
+     */
     private void setTableValues() {
         ObservableList<Equipment> equipmentss = FXCollections.observableArrayList(EquipmentManagerFactory.getEquipmentManager()
                 .findAllEquipment(new GenericType<Set<Equipment>>() {
@@ -357,6 +463,13 @@ public class EquipmentViewController {
         equipmentTable.setItems(equipmentss);
     }
 
+    /**
+     * This method gets all the equipments with the selected filter stored in
+     * the database and load them to the equipment table, it is used to load the
+     * equipment when you only want to search them by use
+     *
+     * @param filtroUso The use you want to search for
+     */
     private void setTableValuesWithFilters(String filtroUso) {
         filtroUso = filtroUso.toLowerCase();
         ObservableList<Equipment> equipmentss = FXCollections.observableArrayList(EquipmentManagerFactory.getEquipmentManager()
@@ -366,6 +479,15 @@ public class EquipmentViewController {
         equipmentTable.setItems(equipmentss);
     }
 
+    /**
+     * This method gets all the equipments with the selected filter and the name
+     * stored in the database and load them to the equipment table, it is used
+     * to load the equipment when you want to search them by use and name
+     * simultaneously
+     *
+     * @param filtroUso The use you want to search for
+     * @param buscador The name you want to look for
+     */
     private void setTableValuesWithFilters(String filtroUso, String buscador) {
         filtroUso = filtroUso.toLowerCase();
         ObservableList<Equipment> equipmentsWithFilter = FXCollections.observableArrayList(EquipmentManagerFactory.getEquipmentManager()
@@ -374,6 +496,13 @@ public class EquipmentViewController {
         equipmentTable.setItems(equipmentsWithFilter);
     }
 
+    /**
+     * This method gets all the equipments with the introduced name stored in
+     * the database and load them to the equipment table, it is used to load the
+     * equipment when you only want to search them by name
+     *
+     * @param buscador The text introduced on the Search textfield
+     */
     private void setTableValuesWithText(String buscador) {
         ObservableList<Equipment> equipmentsWithFilter = FXCollections.observableArrayList(EquipmentManagerFactory.getEquipmentManager()
                 .findEquipmentByName(new GenericType<Set<Equipment>>() {
